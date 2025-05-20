e603649308918e495b02f1c331d772e2ab1018c0
Steve B <stevegbaca@gmail.com>
2025-05-20 12:49:17 -0600

    Test random

--- poc/main.go
+++ poc/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Good Stevening!")
+	url := "https://locations.patch3s.dev/api/random"
+	forecast, _ := fetchForecast(url)
 
+	fmt.Println("main.go@18 waldo: forecast", forecast)
 }
 
 func main() {
@@ -17,3 +22,23 @@ func main() {
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
+func fetchForecast(url string) (map[string]interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
